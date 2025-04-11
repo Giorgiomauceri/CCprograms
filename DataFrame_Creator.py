7ec65587fb3df9254b1f68b06bd81284acb21d13
Giorgiomauceri <121715795+Giorgiomauceri@users.noreply.github.com>
2025-04-11 14:06:14 +0200

    Add files via upload

--- DataFrame_Creator.py
+++ DataFrame_Creator.py
@@ -16,9 +16,9 @@ for i in range(len(DataFrameIds)):
     if ((DataFrameIds[i]>0 and df['sim_pt'][0][DataFrameIds[i]]>0.9 and abs(df['sim_eta'][0][DataFrameIds[i]])<2.4) or DataFrameIds[i]<0):
         Check.append(i)
 DataFrameIds = [DataFrameIds[trk] for trk in Check]
-d = {'truth':[], 'trk_dxy':[], 'trk_dz':[], 'trk_pt':[], 'trk_eta':[], 'trk_nChi2':[], 'trk_nPixel':[]}
+d = {'truth':[], 'trk_dxy':[], 'trk_dz':[], 'trk_pt':[], 'trk_eta':[], 'trk_nChi2':[], 'trk_nPixel':[], 'trk_inner_px':[], 'trk_inner_py':[], 'trk_inner_pz':[], 'trk_inner_pt':[], 'trk_outer_px':[], 'trk_outer_py':[], 'trk_outer_pz':[], 'trk_outer_pt':[], 'trk_ndof':[]}
 DataBase = pd.DataFrame(d)
-f = {'truth':[1], 'trk_dxy':[1], 'trk_dz':[1], 'trk_pt':[1], 'trk_eta':[1], 'trk_nChi2':[1], 'trk_nPixel':[1]}
+f = {'truth':[1], 'trk_dxy':[1], 'trk_dz':[1], 'trk_pt':[1], 'trk_eta':[1], 'trk_nChi2':[1], 'trk_nPixel':[1], 'trk_inner_px':[1], 'trk_inner_py':[1], 'trk_inner_pz':[1], 'trk_inner_pt':[1], 'trk_outer_px':[1], 'trk_outer_py':[1], 'trk_outer_pz':[1], 'trk_outer_pt':[1], 'trk_ndof':[1]}
 count=0
 for ev in range(len(df)):
     temp = ak.argmax(df['trk_simTrkIdx'][ev], keepdims=True, axis=-1, mask_identity=False)
@@ -28,7 +28,7 @@ for ev in range(len(df)):
     for i in range(len(DataFrameIds)):
         #print(i)
         if ((DataFrameIds[i]>0 and df['sim_pt'][ev][DataFrameIds[i]]>0.9 and abs(df['sim_eta'][ev][DataFrameIds[i]])<2.4) or DataFrameIds[i]<0):
-            f = {'truth':[bool(DataFrameIds[i]+1)], 'trk_dxy':[df["trk_dxy"][ev][i]], 'trk_dz':[df["trk_dz"][ev][i]], 'trk_pt':[df["trk_pt"][ev][i]], 'trk_eta':[df["trk_eta"][ev][i]], 'trk_nChi2':[df["trk_nChi2"][ev][i]], 'trk_nPixel':[df["trk_nPixel"][ev][i]]}
+            f = {'truth':[bool(DataFrameIds[i]+1)], 'trk_dxy':[df["trk_dxy"][ev][i]], 'trk_dz':[df["trk_dz"][ev][i]], 'trk_pt':[df["trk_pt"][ev][i]], 'trk_eta':[df["trk_eta"][ev][i]], 'trk_nChi2':[df["trk_nChi2"][ev][i]], 'trk_nPixel':[df["trk_nPixel"][ev][i]], 'trk_inner_px':[df["trk_inner_px"][ev][i]], 'trk_inner_py':[df["trk_inner_py"][ev][i]], 'trk_inner_pz':[df["trk_inner_pz"][ev][i]], 'trk_inner_pt':[df["trk_inner_pt"][ev][i]], 'trk_outer_px':[df["trk_outer_px"][ev][i]], 'trk_outer_py':[df["trk_outer_py"][ev][i]], 'trk_outer_pz':[df["trk_outer_pz"][ev][i]], 'trk_outer_pt':[df["trk_outer_pt"][ev][i]], 'trk_ndof':[df["trk_ndof"][ev][i]]}
             DataBase.loc[count]=f
             #print(ev, i, DataFrameIds[i], df["trk_dxy"][ev][i], df["trk_dz"][ev][i], df["trk_pt"][ev][i], df["trk_eta"][ev][i], df["trk_nChi2"][ev][i], df["trk_nPixel"][ev][i])
             count+=1
@@ -52,6 +52,15 @@ for i in range(TrData.shape[0]):
     data[i, 3] = TrData['trk_eta'][i]
     data[i, 4] = TrData['trk_nChi2'][i]
     data[i, 5] = TrData['trk_nPixel'][i]
+    data[i, 6] = TrData['trk_inner_px'][i]
+    data[i, 7] = TrData['trk_inner_py'][i]
+    data[i, 8] = TrData['trk_inner_pz'][i]
+    data[i, 9] = TrData['trk_inner_pt'][i]
+    data[i, 10] = TrData['trk_outer_px'][i]
+    data[i, 11] = TrData['trk_outer_py'][i]
+    data[i, 12] = TrData['trk_outer_pz'][i]
+    data[i, 13] = TrData['trk_outer_pt'][i]
+    data[i, 14] = TrData['trk_ndof'][i]
     target[i]  = TrData['truth'][i]
 
 data1 = np.zeros((ValData.shape[0], ValData.shape[1]-1))
@@ -63,8 +72,17 @@ for i in range(ValData.shape[0]):
     data1[i, 3] = ValData['trk_eta'][TrData.shape[0]+i-1]
     data1[i, 4] = ValData['trk_nChi2'][TrData.shape[0]+i-1]
     data1[i, 5] = ValData['trk_nPixel'][TrData.shape[0]+i-1]
+    data1[i, 6] = ValData['trk_inner_px'][TrData.shape[0]+i-1]
+    data1[i, 7] = ValData['trk_inner_py'][TrData.shape[0]+i-1]
+    data1[i, 8] = ValData['trk_inner_pz'][TrData.shape[0]+i-1]
+    data1[i, 9] = ValData['trk_inner_pt'][TrData.shape[0]+i-1]
+    data1[i, 10] = ValData['trk_outer_px'][TrData.shape[0]+i-1]
+    data1[i, 11] = ValData['trk_outer_py'][TrData.shape[0]+i-1]
+    data1[i, 12] = ValData['trk_outer_pz'][TrData.shape[0]+i-1]
+    data1[i, 13] = ValData['trk_outer_pt'][TrData.shape[0]+i-1]
+    data1[i, 14] = ValData['trk_ndof'][TrData.shape[0]+i-1]
     target1[i]  = ValData['truth'][TrData.shape[0]+i-1]
-
+    
 #------------------------------------------------------------------------------------------
 print("  Saving files...")
 np.savetxt('tempData/Training_Data.dat', data)
--- NeuralNetwork_Trainer.py
+++ NeuralNetwork_Trainer.py
@@ -1,3 +1,6 @@
+from termcolor import colored
+print(colored("  NeuralNetwork_Trainer started! \n  Importing Packages...", "green"))
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import torch
@@ -5,41 +8,43 @@ import awkward as ak
 import numpy as np
 import uproot
 import sklearn as sk
+from termcolor import colored
 
-print("  NeuralNetwork_Trainer started! \n  Importing Arrays from files...")
+print(colored("  Importing Arrays from files...", "green"))
 target =  np.loadtxt("tempData/Training_Truth.dat")
 data = np.loadtxt("tempData/Training_Data.dat")
 target1 = np.loadtxt("tempData/Test_Truth.dat")
 data1 = np.loadtxt("tempData/Test_Data.dat")
 
 #------------------------------------------------------------------------------------------
-print("  Creating Training and Testing Tensors...")
+print(colored("  Creating Training and Testing Tensors...", "green"))
 target = torch.from_numpy(target).type(torch.float)
 data = torch.from_numpy(data).type(torch.float)
 target1 = torch.from_numpy(target1).type(torch.float)
 data1 = torch.from_numpy(data1).type(torch.float)
 
 #------------------------------------------------------------------------------------------
-print("  Creating Neural Network Model...")
+print(colored("  Creating Neural Network Model...", "green"))
 class TrackAnalysisModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer_1 = torch.nn.Linear(in_features=6, out_features=30)
-        self.layer_2 = torch.nn.Linear(in_features=30, out_features=30)
-        self.layer_3 = torch.nn.Linear(in_features=30, out_features=30)
-        self.layer_4 = torch.nn.Linear(in_features=30, out_features=1)
+        self.layer_1 = torch.nn.Linear(in_features=15, out_features=70)
+        self.layer_2 = torch.nn.Linear(in_features=70, out_features=70)
+        self.layer_3 = torch.nn.Linear(in_features=70, out_features=70)
+        self.layer_4 = torch.nn.Linear(in_features=70, out_features=1)
         self.relu = torch.nn.ReLU() # <- add in ReLU activation function
+        self.dropout = torch.nn.Dropout(p=0.5) # <- add in dropout function
         # Can also put sigmoid in the model 
         # This would mean you don't need to use it on the predictions
         # self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
       # Intersperse the ReLU activation function between layers
-       return self.layer_4(self.relu(self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))))
+       return self.layer_4(self.dropout(self.relu(self.layer_3(self.dropout(self.relu(self.layer_2(self.dropout(self.relu(self.layer_1(x))))))))))
        #return self.layer_6(self.relu(self.layer_5(self.relu(self.layer_4(self.relu(self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))))))))
 
 #------------------------------------------------------------------------------------------
-print("  Analysis Tools Setup...")
+print(colored("  Analysis Tools Setup...", "green"))
 # Calculate accuracy (a classification metric)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = TrackAnalysisModel().to(device)
@@ -54,15 +59,18 @@ loss_fn = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
 #------------------------------------------------------------------------------------------
-print("  Training the Neural Network...")
+print(colored("  Training the Neural Network...", "green"))
 # Fit the model
 torch.manual_seed(42)
-epochs = 6000
+epochs = 6001
 
 # Put all data on target device
 data, target = data.to(device), target.to(device)
 data1, target1 = data1.to(device), target1.to(device)
 
+current_val=0
+prev_val=0
+LOSS = np.zeros((3, epochs))
 for epoch in range(epochs):
     # 1. Forward pass
     y_logits = model(data).squeeze()
@@ -93,21 +101,41 @@ for epoch in range(epochs):
       test_acc = accuracy_fn(y_true=target1,
                              y_pred=test_pred)
 
+    LOSS[0,int(epoch)] = epoch
+    LOSS[1,int(epoch)] = loss
+    LOSS[2,int(epoch)] = test_loss    
     # Print out what's happening
     if epoch % 200 == 0:
-        print(f"    Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
+        print( f"    Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
+
+    if epoch % 100 == 0 and epoch>100:
+        current_val=np.average(LOSS[2, epoch-100:epoch])
+        if np.abs(current_val-prev_val)<=0.0002:
+            print(colored("  Plateau reached, terminating training...", "blue"))
+            break
+        else:
+            prev_val=current_val
+
+LOSS = LOSS[:, :epoch]
+
 
 #------------------------------------------------------------------------------------------
-print("  Printing ROC curve:")
+print(colored("  Printing ROC curve:", "green"))
 PredVal = torch.sigmoid(model(data1).squeeze())
 PredVal = PredVal.detach().numpy()
 fpr, tpr, thr= sk.metrics.roc_curve(target1, PredVal, drop_intermediate=False)
 plt.plot(fpr, tpr)
 plt.show()
-print("  ROC curve knee: ", fpr[1])
+
+#------------------------------------------------------------------------------------------
+print(colored("  Printing Loss Evolution:", "green"))
+plt.plot(LOSS[0,:], LOSS[1,:], color='red', linewidth=0.7)
+plt.plot(LOSS[0,:], LOSS[2,:], color='blue', linewidth=0.7)
+plt.yscale('log')
+plt.show()
 
 ROCdata = np.column_stack([fpr, tpr])
 FakeData = np.column_stack([PredVal, target1])
 np.savetxt("tempData/ROC_NN3x30.dat" , ROCdata)
 np.savetxt("tempData/FAKE_NN3x30.dat", FakeData)
-print("  Finished")
+print(colored("  Finished", "green"))
